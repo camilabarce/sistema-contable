02b959ec1ba8abef4a05210a0e2257fbc1f19907
Christian Mathias Rincón <112495195+ChristianRincon@users.noreply.github.com>
2023-09-05 11:13:59 -0300

    Permiso concedido para consumir API´s con Angular

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // Configuramos nuestro servidor
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
 const path = require('path'); // Para concatenar ruta del index.html
 const cuentaController = require('./Controllers/cuentaController'); // Orquestador.
@@ -12,7 +12,7 @@ app.set('port', process.env.PORT || 3000); // Asignamos un puerto disponible por
 const port = app.get('port');
 
 // Middlewares (Servicios intermedios)
-app.use(cors()) //Permite comunicar a nuestro servidor (http://localhost:3000) con Angular (http://localhost:4200)
+app.use(cors()); //Permite comunicar a nuestro servidor (http://localhost:3000) con Angular (http://localhost:4200)
 app.use(morgan("dev")); //Genera los status y tiempo de respuesta por consola cuando detecta eventos en la página.
 app.use(express.json()); //Para interpretar el formato JSON automáticamente (Evitamos especificar el Content-Type="text/json").
 
